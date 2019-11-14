2263f285d7f83eecf3a393089646c6345ddf54ff
G. Sun (4th Year - Woodland) <gs534@air209>
2019-11-14 12:13:42 +0000

    Tidy up

--- SelfAtten.py
+++ SelfAtten.py
@@ -3,8 +3,14 @@ from torch import cat, rand, zeros, matmul, eye, set_printoptions
 from torch.autograd import Variable
 
 class SelfAttenModel(nn.Module):
-    '''Implementation of self-attentive layer'''
+    """Implementation of self-attentive layer"""
     def __init__(self, ninp, ninterm, nweights, dropinter=0.2, dropatt=0):
+        """ninp: input hidden state dimension
+           ninterm: intermediate dimension
+           nweights: number of attention weights
+           dropinter: intermediate stage dropout rate
+           dropatt: attention dropout rate
+        """
         super(SelfAttenModel, self).__init__()
         self.layer1 = nn.Linear(ninp, ninterm*nweights, bias=False)
         self.tanh = nn.Tanh()
@@ -20,19 +26,24 @@ class SelfAttenModel(nn.Module):
         self.layer1.weight.data.uniform_(-initrange, initrange)
         self.layer2.weight.data.uniform_(-initrange, initrange)
         
-    def forward(self, uttemb, scale=1, device='cuda', writeout=False, wordlevel=False):
+    def forward(self, embs, scale=1, device='cuda', wordlevel=False):
+        """embs: input embeddings, could be word or segment embedding
+	   scale: scale of the penalty term
+	   device: cuda or cpu
+	   wordlevel: what level are the embeddings at
+	"""
         if not wordlevel:
-            if uttemb.size(1) % self.ninp != 0:
+            if embs.size(1) % self.ninp != 0:
                 print('Splitting of input embedding is invalid!')
                 raise
-            uttemb = uttemb.view(uttemb.size(0), -1, self.ninp)
-        intermediate = self.tanh(self.layer1(uttemb))
+            embs = embs.view(embs.size(0), -1, self.ninp)
+        intermediate = self.tanh(self.layer1(embs))
         annotmatrix = self.softmax(self.layer2(intermediate))
-        totaloutput = matmul(annotmatrix.transpose(1,2), uttemb)
+        totaloutput = matmul(annotmatrix.transpose(1,2), embs)
         ATA = matmul(annotmatrix.transpose(1,2), annotmatrix)
         I = eye(self.nweights).to(device)
         penalty = scale * ((ATA - I.expand_as(ATA)) ** 2).sum()
-        return totaloutput.view(uttemb.size(0), -1), penalty
+        return totaloutput.view(embs.size(0), -1), penalty
 
 if __name__ == "__main__":
     a = rand(3,2,5)
