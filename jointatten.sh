bad84be38948e547156fb6851a02c330bdc676f0
G. Sun (4th Year - Woodland) <gs534@air208.eng.cam.ac.uk>
2019-11-20 15:44:40 +0000

    Use error sampling

--- jointatten.sh
+++ jointatten.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0 #${X_SGE_CUDA_DEVICE}
 export PATH="/home/miproj/urop.2018/gs534/Software/anaconda3/bin:$PATH"
 
-exp_no=1
+exp_no=2
 model_name="atten"
 prev_len=72
 post_len=72
@@ -20,12 +20,12 @@ python jointtrain_singleseg.py \
     --FLlr 0.5 \
     --clip 0.25 \
     --FLvclip 0.5 \
-    --batchsize 1 \
+    --batchsize 64 \
     --wdecay 2e-6 \
     --bptt 12 \
     --naux 512 \
     --reset 1 \
-    --epochs 30 \
+    --epochs 39 \
     --maxlen_prev $prev_len \
     --maxlen_post $post_len \
     --FLvmodel Flvmodel/model.pt \
@@ -37,6 +37,7 @@ python jointtrain_singleseg.py \
     --useatten \
     --nhead 1 \
     --alpha 0.0000 \
-    # --use_sampling \
-    # --errorfile error_sampling/work/confusions.txt \
-    # --reference error_sampling/train.ref \
+    --use_sampling \
+    --errorfile error_sampling/work/confusions.txt \
+    --reference error_sampling/train.ref \
+    --ratio 5 \
